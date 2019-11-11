d0d13e1ff38244c3d3247d55df24b692b3d61915
Daren Liang <darenliang@outlook.com>
2019-11-11 17:19:16 -0500

    Add config description for client timeout

--- config.go
+++ config.go
@@ -4,4 +4,5 @@ package jikan
 const Endpoint = "https://api.jikan.moe/v3"
 
 // ClientTimeout specifies the timeout value in seconds of the http client
+// You might want to increase this number if you have a slow connection
 const ClientTimeout = 20
