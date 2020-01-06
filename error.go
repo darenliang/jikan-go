77ab2cbf1830dccd7e4e62be4e59ee35c98279b6
Daren Liang <darenliang@outlook.com>
2020-01-06 07:48:19 -0500

    Add comment to getResultError
    Add a comment detailing that getResultError should only be responsible for printing the error regardless of response being an error.

--- error.go
+++ error.go
@@ -2,7 +2,8 @@ package jikan
 
 import "fmt"
 
-// getResultError returns an error with information from result
+// getResultError returns an error with information from jikan response.
+// Note that getResultError does not whether or not an error has occured.
 func getResultError(result map[string]interface{}) error {
 	return fmt.Errorf("error %v, %v, %v", result["status"], result["message"], result["error"])
 }
